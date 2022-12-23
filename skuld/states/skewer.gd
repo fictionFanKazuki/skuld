61634da31e5454671c5ec77dcb43c27083d39e1f
fictionFanKazuki <77258421+fictionFanKazuki@users.noreply.github.com>
2022-12-23 16:29:16 +0400

    m tired

--- skuld/states/skewer.gd
+++ skuld/states/skewer.gd
@@ -2,13 +2,15 @@ extends CharacterState
 
 func _frame_10():
 	var position = host.get_hurtbox_center()
+	var proj_x = position["x"]
+	var proj_y = position["y"] - 200
 	var target = host.opponent.get_hurtbox_center()
-	var direction = xy_to_dir(target["x"], target["y"])
+	var direction = xy_to_dir(target["x"] - proj_x, target["y"] - proj_y)
 	
 	host.spawn_object(
 		preload("res://characters/skuld/projectiles/large_clock_hand/LargeClockHand.tscn"),
-		position["x"],
-		position["y"] - 200,
-		false,
+		proj_x + data.x,
+		proj_y,
+		true,
 		{"dir": direction}
 	)
--- skuld/states/summon_small_clock_hand.gd
+++ skuld/states/summon_small_clock_hand.gd
@@ -6,19 +6,19 @@ func _frame_0():
 	# these are just default spawn positions in case something goes wrong
 	var proj_x = -200
 	var proj_y = -200
-	#var lockon = host.opponent.get_hurtbox_center()
+	var lockon = host.opponent.get_hurtbox_center()
 	
 	# cool linear algebra stuff i stole that you'll probably understand better
 	var dir = xy_to_dir(data["x"], data["y"])
-	#var shot_dir = fixed.normalized_vec_times(dir.x, dir.y, "-1")
-	#var offset = fixed.normalized_vec_times(dir.x, dir.y, "-1")
+	var shot_dir = fixed.normalized_vec_times(dir.x, dir.y, "-1")
+	var offset = fixed.normalized_vec_times(dir.x, dir.y, "-1")
 	
-	#proj_x = int(fixed.add(str(lockon.x), offset.x))
-	#proj_y = int(fixed.add(str(lockon.y), offset.y))
+	proj_x = int(fixed.add(str(lockon.x), offset.x))
+	proj_y = int(fixed.add(str(lockon.y), offset.y))
 	
 	# okay so this gives us an index we can use to access objects associated with our character
 	# (spawn_clock_hand is defined in skuld.gd)
-	projectile = host.spawn_small_clock_hand(proj_x, proj_y, dir)
+	projectile = host.spawn_small_clock_hand(proj_x, proj_y, shot_dir)
 	
 #func _frame_3():
 	# which we make use over here to be able to access the ClockHands object defined by the functions in
